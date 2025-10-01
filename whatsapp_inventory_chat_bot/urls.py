7ccfd7a4fe444f776d47ba1c2c905e1a5845a631
Dennis wambua <muasyathegreat4@gmail.com>
2025-10-01 08:59:26 +0300

    healthcheck path added

--- whatsapp_inventory_chat_bot/urls.py
+++ whatsapp_inventory_chat_bot/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('webhook_receiver.urls'))
+    path('api/',include('webhook_receiver.urls')),
+    path('health', health, name='health'),
 ]
